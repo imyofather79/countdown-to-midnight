2710c50013d254dcc96ddaab947564017c4016bf
imyofather79 <chunsum708@gmail.com>
2021-02-26 08:36:10 -0700

    commit tree 729dcd7539350e4c9d79ad239aaa823159a6f685

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,27 @@
 #write your code here
+# use while loop
+# use -= subtract and assign operator
+# "#{number} SECOND(S)!"
+#return "HAPPY NEW YEAR!"
 
-def countdown
+
+def countdown(number)
+while number > 0
+puts "#{number} SECOND(S)!"
+number -= 1
 end
+"HAPPY NEW YEAR!"
+end
+
+
+
+def countdown_with_sleep(number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        
+sleep 1
+
+        number -= 1
+        end
+        "HAPPY NEW YEAR!"
+    end
